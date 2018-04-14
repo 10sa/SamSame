8d5579d3dd04bbf6be85a4ba70cfed65301b924d
10sa <zenkins26@gmail.com>
2018-04-14 14:32:17 +0900

    Add DBConnector API, mysql lib.

---
+++ SamSame/Apis/DBConnector.js
@@ -0,0 +1,30 @@
+﻿var mysql = require("mysql");
+var dbConfigs = require("/configs/configs.js").dev;
+
+module.exports = function () {
+	return {
+		init: function () {
+			return mysql.createConnection({
+				host: dbConfigs.host,
+				port: dbConfigs.port,
+				user: dbConfigs.user,
+				password: dbConfigs.password,
+				database: dbConfigs.database
+			});
+		},
+
+		connect: function (connection) {
+			connection.connect(function (error) {
+				if (error)
+					console.error("DB Connection Error!");
+			});
+		},
+
+		getConnection: function () {
+			var temp = this.init();
+			this.connect(temp);
+
+			return temp;
+		}
+	}
+}
