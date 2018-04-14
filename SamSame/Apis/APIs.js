d3dea26c0c27c6b780b201dbd8b3fb3356a428e4
10sa <zenkins26@gmail.com>
2018-04-14 14:35:54 +0900

    Add passport, express / Init routers.

---
+++ SamSame/Apis/APIs.js
@@ -0,0 +1,4 @@
+﻿var express = require("express");
+var router = express.Router();
+
+module.exports = router;
---
+++ SamSame/Apis/Pages.js
@@ -0,0 +1,7 @@
+﻿var express = require("express");
+var router = express.Router();
+
+var passport = require("passport");
+
+
+module.exports = router;
