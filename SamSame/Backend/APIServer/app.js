996683b58f35c74038141c9e6d5899157af71993
MinchanKim <kimab7890@gmail.com>
2018-05-13 10:00:09 +0900

    .env 파일 추가 config 파일 -> 폴더로 변경(여러 설정값들 분류 목적)

--- SamSame/Backend/APIServer/app.js
+++ SamSame/Backend/APIServer/app.js
@@ -2,12 +2,35 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const http = require('http');
+const https = require('https');
 
 /* Configure Setting */
-require('./config')();
+const config = require('./config')();
+// const option = config.httpsOption;
 
 /* Express Setting */
 app.use(bodyParser.urlencoded({ extended : false }));
 app.use(bodyParser.raw());
 
-// Add api Router
+/* Api Router */
+const api = require('./api');
+app.use('/', api);
+
+/* Run Server */
+http.createServer(app).listen(
+    process.env.HTTP_PORT, 
+    () => {
+        console.log('Http server starts on port ' + 
+            process.env.HTTP_PORT);
+    }
+);
+/*
+https.createServer(option, app).listen(
+    process.env.HTTPS_PORT,
+    () => {
+        console.log('Https server starts on port' +
+            process.env.HTTPS_PORT);
+    }
+);
+*/
