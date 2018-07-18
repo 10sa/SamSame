8bfd7a2361da8f04aaaab68589e387b53ddd347a
MinchanKim <syubsyubboy@gmail.com>
2018-07-18 20:02:57 +0900

    로그인 버그 해결(미완)

--- Backend/public/resources/js/api-account.js
+++ Backend/public/resources/js/api-account.js
@@ -3,11 +3,11 @@
 
 var account = {};
 account.login = function (username, password, callback) {
-	Util.CallAPI(String.format("username={0}&password={1}", username, password), "/apis/account/login", "POST", callback);
+	Util.CallAPI(String.format("email={0}&password={1}", username, password), "/apis/account/login", "POST", callback);
 }
 
 account.logout = function (callback) {
-	Util.CallAPI("", "/apis/account/logout", "POST", callback);
+	Util.CallAPI("", "/apis/account/logout", "GET", callback);
 }
 
 account.register = function (email, password, callback) {
@@ -19,5 +19,5 @@ account.userProfile = function (callback) {
 }
 
 account.userProfile = function (key, value, callback) {
-	Util.CallAPI(String.format("key={0}&value={1}", key, value), "/apis/account/userProfile", "POST", callback);
+	Util.CallAPI(String.format("email={0}&username={1}", key, value), "/apis/account/userProfile", "POST", callback);
 }
